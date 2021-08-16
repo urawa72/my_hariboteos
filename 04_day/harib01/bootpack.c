fb04a1db7f1803805ab48b922a9f1ef6a8bf8b30
urawa72 <urawa72@gmail.com>
2021-08-16 17:27:41 +0900

    04_day: hairb01d

--- 04_day/harib01/bootpack.c
+++ 04_day/harib01/bootpack.c
@@ -4,10 +4,10 @@ extern void write_mem8(int addr, int data);
 void HariMain(void) {
   int i;
   char *p;
+  p = (char *)0xa0000;
 
-  for (i = 0xa0000; i <= 0xaffff; i++) {
-    p = (char *)i;
-    *p = i & 0x0f;
+  for (i = 0; i <= 0xaffff; i++) {
+    *(p + i) = i & 0x0f;
   }
 
   for (;;) {
