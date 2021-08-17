09aac2689e86f37f9938df7de498865352bb6020
urawa72 <urawa72@gmail.com>
2021-08-17 20:49:31 +0900

    05_day: harib02b

--- 05_day/bootpack.c
+++ 05_day/bootpack.c
@@ -4,9 +4,10 @@ void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
 
-void init_pallet(void);
-void set_pallet(int start, int end, unsigned char *rgb);
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(char *vram, int x, int y);
 
 #define COL8_000000 0
 #define COL8_FF0000 1
@@ -25,43 +26,31 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 #define COL8_008484 14
 #define COL8_848484 15
 
+struct BOOTINFO {
+  char cyls, leds, vmode, reserve;
+  short scrnx, scrny;
+  char *vram;
+};
+
 void HariMain(void) {
   char *vram;
   int xsize, ysize;
-	short *binfo_scrnx, *binfo_scrny;
-	int *binfo_vram;
+  struct BOOTINFO *binfo;
 
-  init_pallet();
-	binfo_scrnx = (short *)0x0ff4;
-	binfo_scrny = (short *)0x0ff6;
-	binfo_vram = (int *)0x0ff8;
-  xsize = *binfo_scrnx;
-  ysize = *binfo_scrny;
-  vram = (char *)*binfo_vram;
+  init_palette();
+  binfo = (struct BOOTINFO *)0x0ff0;
+  xsize = (*binfo).scrnx;
+  ysize = (*binfo).scrny;
+  vram = (*binfo).vram;
 
-  boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
-  boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
-  boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
-  boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
-
-  boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
-  boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
-  boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
-  boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
-  boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
-  boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
-
-  boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
-  boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
-  boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
-  boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+	init_screen(vram, xsize, ysize);
 
   for (;;) {
     io_hlt();
   }
 }
 
-void init_pallet(void) {
+void init_palette(void) {
   static unsigned char table_rgb[16 * 3] = {
       0x00, 0x00, 0x00,  //  0:black
       0xff, 0x00, 0x00,  //  1:red
@@ -80,11 +69,11 @@ void init_pallet(void) {
       0x00, 0x84, 0x84,  // 14:dark water blue
       0x84, 0x84, 0x84   // 15:dark gray
   };
-  set_pallet(0, 15, table_rgb);
+  set_palette(0, 15, table_rgb);
   return;
 }
 
-void set_pallet(int start, int end, unsigned char *rgb) {
+void set_palette(int start, int end, unsigned char *rgb) {
   int i, eflags;
   eflags = io_load_eflags();
   io_cli();
@@ -106,5 +95,25 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
       vram[y * xsize + x] = c;
     }
   }
+  return;
+}
+
+void init_screen(char *vram, int x, int y) {
+  boxfill8(vram, x, COL8_008484, 0, 0, x - 1, y - 29);
+  boxfill8(vram, x, COL8_C6C6C6, 0, y - 28, x - 1, y - 28);
+  boxfill8(vram, x, COL8_FFFFFF, 0, y - 27, x - 1, y - 27);
+  boxfill8(vram, x, COL8_C6C6C6, 0, y - 26, x - 1, y - 1);
+
+  boxfill8(vram, x, COL8_FFFFFF, 3, y - 24, 59, y - 24);
+  boxfill8(vram, x, COL8_FFFFFF, 2, y - 24, 2, y - 4);
+  boxfill8(vram, x, COL8_848484, 3, y - 4, 59, y - 4);
+  boxfill8(vram, x, COL8_848484, 59, y - 23, 59, y - 5);
+  boxfill8(vram, x, COL8_000000, 2, y - 3, 59, y - 3);
+  boxfill8(vram, x, COL8_000000, 60, y - 24, 60, y - 3);
+
+  boxfill8(vram, x, COL8_848484, x - 47, y - 24, x - 4, y - 24);
+  boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y - 4);
+  boxfill8(vram, x, COL8_FFFFFF, x - 47, y - 3, x - 4, y - 3);
+  boxfill8(vram, x, COL8_FFFFFF, x - 3, y - 24, x - 3, y - 3);
   return;
 }
