3a0baced440fb325a5b10eb3f4e1ddb941b5aca2
urawa72 <urawa72@gmail.com>
2021-08-18 13:29:47 +0900

    05_day: harib02g

--- 05_day/bootpack.c
+++ 05_day/bootpack.c
@@ -10,6 +10,7 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+extern void sprintf(char *str, const char *fmt, ...);
 
 #define COL8_000000 0
 #define COL8_FF0000 1
@@ -36,13 +37,16 @@ struct BOOTINFO {
 
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
-  extern char hankaku[4096];
+	char s[40];
 
   init_palette();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+
 	putfonts8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "ABC 123");
 	putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
 	putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
+	sprintf(s, "scrnx = %d", binfo->scrnx);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
   for (;;) {
     io_hlt();
