76e2098ca64d1c8151b82acdded3bb6301014380
urawa72 <urawa72@gmail.com>
2021-08-22 14:23:53 +0900

    07_day: harib04c

--- 07_day/bootpack.c
+++ 07_day/bootpack.c
@@ -5,17 +5,17 @@ extern struct KEYBUF keybuf;
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   char s[40], mcursor[256];
-  int mx, my, i;
+  int mx, my, i, j;
 
-	// initialize IDT/PIC
+  // initialize IDT/PIC
   init_gdtidt();
   init_pic();
 
-	// release CPU interrupt prohibition
-	io_sti();
+  // release CPU interrupt prohibition
+  io_sti();
 
-	io_out8(PIC0_IMR, 0xf9); // allow PIC1 keyboard (11111001)
-	io_out8(PIC1_IMR, 0xef); // allow mouse (11101111)
+  io_out8(PIC0_IMR, 0xf9);  // allow PIC1 keyboard (11111001)
+  io_out8(PIC1_IMR, 0xef);  // allow mouse (11101111)
 
   init_palette();
   init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -27,16 +27,19 @@ void HariMain(void) {
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
   for (;;) {
-		io_cli();
-		if (keybuf.flag == 0) {
-			io_stihlt();
-		}else {
-			i = keybuf.data;
-			keybuf.flag = 0;
-			io_sti();
-			sprintf(s, "%x", i);
-			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-		}
+    io_cli();
+    if (keybuf.next == 0) {
+      io_stihlt();
+    } else {
+      i = keybuf.data[0];
+      keybuf.next--;
+      for (j = 0; j < keybuf.next; j++) {
+        keybuf.data[j] = keybuf.data[j + 1];
+      }
+      io_sti();
+      sprintf(s, "%x", i);
+      boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+      putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+    }
   }
 }
