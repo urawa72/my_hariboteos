51162761810c69916563d62d8d21ca137fdfd9eb
urawa72 <urawa72@gmail.com>
2021-08-22 15:17:39 +0900

    08_day: harib05a

--- 08_day/bootpack.c
+++ 08_day/bootpack.c
@@ -8,6 +8,7 @@ void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
   int mx, my, i;
+  unsigned char mouse_dbuf[3], mouse_phase;
 
   // initialize IDT/PIC
   init_gdtidt();
@@ -48,9 +49,23 @@ void HariMain(void) {
       } else if (fifo8_status(&mousefifo) != 0) {
         i = fifo8_get(&mousefifo);
         io_sti();
-        my_sprintf(s, "%x", i);
-        boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-        putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+        if (mouse_phase == 0) {
+          if (i == 0xfa) {
+            mouse_phase = 1;
+          }
+        } else if (mouse_phase == 1) {
+          mouse_dbuf[0] = i;
+          mouse_phase   = 2;
+        } else if (mouse_phase == 2) {
+          mouse_dbuf[1] = i;
+          mouse_phase   = 3;
+        } else if (mouse_phase == 3) {
+          mouse_dbuf[2] = i;
+          mouse_phase   = 1;
+          my_sprintf(s, "%x %x %x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+          putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+        }
       }
     }
   }
