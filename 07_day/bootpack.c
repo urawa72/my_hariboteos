11db70348aab1c18cdc7f78390011c2a0a200ef9
urawa72 <urawa72@gmail.com>
2021-08-22 14:55:28 +0900

    07_day: harib04f

--- 07_day/bootpack.c
+++ 07_day/bootpack.c
@@ -1,6 +1,8 @@
 #include "bootpack.h"
 
 extern struct FIFO8 keyinfo;
+void enable_mouse(void);
+void init_keyboard(void);
 
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
@@ -14,9 +16,11 @@ void HariMain(void) {
   // release CPU interrupt prohibition
   io_sti();
 
-	fifo8_init(&keyinfo, 32, keybuf);
+  fifo8_init(&keyinfo, 32, keybuf);
   io_out8(PIC0_IMR, 0xf9);  // allow PIC1 keyboard (11111001)
   io_out8(PIC1_IMR, 0xef);  // allow mouse (11101111)
+
+	init_keyboard();
 
   init_palette();
   init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -27,16 +31,53 @@ void HariMain(void) {
   sprintf(s, "(%d %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
+	enable_mouse();
+
   for (;;) {
     io_cli();
     if (fifo8_status(&keyinfo) == 0) {
       io_stihlt();
     } else {
-			i = fifo8_get(&keyinfo);
+      i = fifo8_get(&keyinfo);
       io_sti();
       sprintf(s, "%x", i);
       boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
       putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
     }
   }
+}
+
+#define PORT_KEYDAT 0x0060
+#define PORT_KEYSTA 0x0064
+#define PORT_KEYCMD 0x0064
+#define KEYSTA_SEND_NOTREADY 0x02
+#define KEYCMD_WRITE_MODE 0x60
+#define KBC_MODE 0x47
+
+void wait_KBC_sendready(void) {
+  for (;;) {
+    if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+      break;
+    }
+    return;
+  }
+}
+
+void init_keyboard(void) {
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, KBC_MODE);
+  return;
+}
+
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+void enable_mouse(void){
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	return;
 }
