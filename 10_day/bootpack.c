49117f973b77644c4f9b94bc99a14ccd93f4921f
urawa72 <urawa72@gmail.com>
2021-08-24 13:21:10 +0900

    10_day: harib07c

--- 10_day/bootpack.c
+++ 10_day/bootpack.c
@@ -7,9 +7,9 @@ void HariMain(void) {
   unsigned int memtotal;
   struct MOUSE_DEC mdec;
   struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
-	struct SHTCTL *shtctl;
-	struct SHEET *sht_back, *sht_mouse;
-	unsigned char *buf_back, buf_mouse[256];
+  struct SHTCTL *shtctl;
+  struct SHEET *sht_back, *sht_mouse;
+  unsigned char *buf_back, buf_mouse[256];
 
   // initialize IDT/PIC
   init_gdtidt();
@@ -32,25 +32,25 @@ void HariMain(void) {
   memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
   init_palette();
-	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
-	sht_back = sheet_alloc(shtctl);
-	sht_mouse = sheet_alloc(shtctl);
-	buf_back = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
-	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
-	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+  shtctl    = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+  sht_back  = sheet_alloc(shtctl);
+  sht_mouse = sheet_alloc(shtctl);
+  buf_back  = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+  sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
+  sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
   init_screen8(buf_back, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(buf_mouse, 99);
-	sheet_slide(shtctl, sht_back, 0, 0);
+  sheet_slide(shtctl, sht_back, 0, 0);
   mx = (binfo->scrnx - 16) / 2;
   my = (binfo->scrny - 28 - 16) / 2;
-	sheet_slide(shtctl, sht_mouse, mx, my);
-	sheet_updown(shtctl, sht_back, 0);
-	sheet_updown(shtctl, sht_mouse, 1);
+  sheet_slide(shtctl, sht_mouse, mx, my);
+  sheet_updown(shtctl, sht_back, 0);
+  sheet_updown(shtctl, sht_mouse, 1);
   my_sprintf(s, "(%d %d)", mx, my);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
   my_sprintf(s, "memory %dMB   free: %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
-	sheet_refresh(shtctl);
+  sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 
   for (;;) {
     io_cli();
@@ -63,7 +63,7 @@ void HariMain(void) {
         my_sprintf(s, "%x", i);
         boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
         putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-				sheet_refresh(shtctl);
+        sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
       } else if (fifo8_status(&mousefifo) != 0) {
         i = fifo8_get(&mousefifo);
         io_sti();
@@ -80,6 +80,7 @@ void HariMain(void) {
           }
           boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
           putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+          sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 
           mx += mdec.x;
           my += mdec.y;
@@ -98,7 +99,8 @@ void HariMain(void) {
           my_sprintf(s, "(%d %d)", mx, my);
           boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
           putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-					sheet_slide(shtctl, sht_mouse, mx, my);
+          sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+          sheet_slide(shtctl, sht_mouse, mx, my);
         }
       }
     }
