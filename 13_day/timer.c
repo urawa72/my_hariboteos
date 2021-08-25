b0ed184fd807bd833fd5be57c467881c2ccdbfe9
urawa72 <urawa72@gmail.com>
2021-08-25 13:23:30 +0900

    chore: add 13_day

---
+++ 13_day/timer.c
@@ -0,0 +1,97 @@
+#include "bootpack.h"
+
+#define PIT_CTRL 0x0043
+#define PIT_CNT0 0x0040
+
+struct TIMERCTL timerctl;
+
+#define TIMER_FLAGS_ALLOC 1  // timer allocated
+#define TIMER_FLAGS_USING 2  // timer working
+
+void init_pit(void) {
+  int i;
+  io_out8(PIT_CTRL, 0x34);
+  io_out8(PIT_CNT0, 0x9c);
+  io_out8(PIT_CNT0, 0x2e);
+  timerctl.count = 0;
+  timerctl.next  = 0xffffffff;  // working timer not exist
+	timerctl.using = 0;
+  for (i = 0; i < MAX_TIMER; i++) {
+    timerctl.timers0[i].flags = 0;  // unused
+  }
+  return;
+}
+
+struct TIMER *timer_alloc(void) {
+  int i;
+  for (i = 0; i < MAX_TIMER; i++) {
+    if (timerctl.timers0[i].flags == 0) {
+      timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+      return &timerctl.timers0[i];
+    }
+  }
+  return 0;  // unused timer not found
+}
+
+void timer_free(struct TIMER *timer) {
+  timer->flags = 0;
+  return;
+}
+
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data) {
+  timer->fifo = fifo;
+  timer->data = data;
+  return;
+}
+
+void timer_settime(struct TIMER *timer, unsigned int timeout) {
+  int e, i, j;
+  timer->timeout = timeout + timerctl.count;
+  timer->flags   = TIMER_FLAGS_USING;
+  e              = io_load_eflags();
+  io_cli();
+  // search for insert
+  for (i = 0; i < timerctl.using; i++) {
+    if (timerctl.timers[i]->timeout >= timer->timeout) {
+      break;
+    }
+  }
+	// slide
+  for (j = timerctl.using; j > i; j--) {
+    timerctl.timers[j] = timerctl.timers[j - 1];
+  }
+	timerctl.using++;
+	// insert into gap
+	timerctl.timers[i] = timer;
+	timerctl.next = timerctl.timers[0]->timeout;
+	io_store_eflags(e);
+	return;
+}
+
+void inthandler20(int *esp) {
+  int i, j;
+  io_out8(PIC0_OCW2, 0x60);  // notify PIC for IRQ-00 reception closed
+  timerctl.count++;
+  if (timerctl.next > timerctl.count) {
+    return;  // not next time yet
+  }
+	for (i = 0; i < timerctl.using; i++) {
+		if (timerctl.timers[i]->timeout > timerctl.count) {
+			break;
+		}
+    // timeout
+		timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
+		fifo8_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+  }
+  // reduce using count by the number of timer was timeout
+	timerctl.using -= i;
+	for (j = 0; j < timerctl.using; j++) {
+		timerctl.timers[j] = timerctl.timers[i + j];
+	}
+	if (timerctl.using > 0) {
+		timerctl.next = timerctl.timers[0]->timeout;
+	} else {
+		timerctl.next = 0xffffffff;
+	}
+  return;
+}
