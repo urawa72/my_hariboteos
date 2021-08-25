4056e6687ded58fbd64a72a3f9eed95298874cf0
urawa72 <urawa72@gmail.com>
2021-08-25 11:11:18 +0900

    12_day: harib09d

--- 12_day/bootpack.c
+++ 12_day/bootpack.c
@@ -4,8 +4,9 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
 
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
-	struct FIFO8 timerfifo;
-  char s[40], keybuf[32], mousebuf[128], timerbuf[8];
+  struct FIFO8 timerfifo, timerfifo2, timerfifo3;
+  char s[40], keybuf[32], mousebuf[128], timerbuf[8], timerbuf2[8], timerbuf3[8];
+  struct TIMER *timer, *timer2, *timer3;
   int mx, my, i;
   unsigned int memtotal;
   struct MOUSE_DEC mdec;
@@ -17,15 +18,26 @@ void HariMain(void) {
   // initialize IDT/PIC
   init_gdtidt();
   init_pic();
-  io_sti(); // release CPU interrupt prohibition
+  io_sti();  // release CPU interrupt prohibition
   fifo8_init(&keyfifo, 32, keybuf);
   fifo8_init(&mousefifo, 128, mousebuf);
-	init_pit();
+  init_pit();
   io_out8(PIC0_IMR, 0xf8);  // allow PIT and PIC1 keyboard (11111000)
   io_out8(PIC1_IMR, 0xef);  // allow mouse (11101111)
 
-	fifo8_init(&timerfifo, 8, timerbuf);
-	settimer(1000, &timerfifo, 1);
+  // timer
+  fifo8_init(&timerfifo, 8, timerbuf);
+  timer = timer_alloc();
+  timer_init(timer, &timerfifo, 1);
+  timer_settime(timer, 1000);
+  fifo8_init(&timerfifo2, 8, timerbuf2);
+  timer2 = timer_alloc();
+  timer_init(timer2, &timerfifo2, 1);
+  timer_settime(timer2, 300);
+  fifo8_init(&timerfifo3, 8, timerbuf2);
+  timer3 = timer_alloc();
+  timer_init(timer3, &timerfifo3, 1);
+  timer_settime(timer3, 50);
 
   init_keyboard();
   enable_mouse(&mdec);
@@ -69,7 +81,7 @@ void HariMain(void) {
     sheet_refresh(sht_win, 40, 28, 120, 44);
 
     io_cli();
-    if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0 + fifo8_status(&timerfifo)) {
+    if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) + fifo8_status(&timerfifo2) + fifo8_status(&timerfifo3) == 0) {
       io_sti();
     } else {
       if (fifo8_status(&keyfifo) != 0) {
@@ -118,10 +130,27 @@ void HariMain(void) {
           sheet_slide(sht_mouse, mx, my);
         }
       } else if (fifo8_status(&timerfifo) != 0) {
-				i = fifo8_get(&timerfifo);
-				io_sti();
-				putfonts8_asc(buf_back, binfo->scrnx, 0, 64, COL8_FFFFFF, "10[sec]");
-				sheet_refresh(sht_back, 0, 64, 56, 80);
+        i = fifo8_get(&timerfifo);
+        io_sti();
+        putfonts8_asc(buf_back, binfo->scrnx, 0, 64, COL8_FFFFFF, "10[sec]");
+        sheet_refresh(sht_back, 0, 64, 56, 80);
+      } else if (fifo8_status(&timerfifo2) != 0) {
+        i = fifo8_get(&timerfifo2);
+        io_sti();
+        putfonts8_asc(buf_back, binfo->scrnx, 0, 80, COL8_FFFFFF, "3[sec]");
+				sheet_refresh(sht_back, 0, 80, 48, 96);
+      } else if (fifo8_status(&timerfifo3) != 0) {
+        i = fifo8_get(&timerfifo3);
+        io_sti();
+        if (i != 0) {
+          timer_init(timer3, &timerfifo3, 0);
+          boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+        } else {
+          timer_init(timer3, &timerfifo3, 1);
+          boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+        }
+        timer_settime(timer3, 50);
+        sheet_refresh(sht_back, 8, 96, 16, 112);
       }
     }
   }
