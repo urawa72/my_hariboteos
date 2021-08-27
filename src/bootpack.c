09edb4d5887b53d7f2a255e697cb35083cd2ba6c
urawa72 <urawa72@gmail.com>
2021-08-27 10:45:20 +0900

    harib12d

--- src/bootpack.c
+++ src/bootpack.c
@@ -115,6 +115,7 @@ void HariMain(void) {
   tss_b.ds     = 1 * 8;
   tss_b.fs     = 1 * 8;
   tss_b.gs     = 1 * 8;
+  *((int *) 0x0fec) = (int) sht_back;
 
   for (;;) {
     // NOTE: The screen freezes without thw following code
@@ -261,18 +262,23 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
 void task_b_main(void) {
   struct FIFO32 fifo;
   struct TIMER *timer_ts;
-  int i, fifobuf[128];
+  int i, fifobuf[128], count = 0;
+  char s[11];
+  struct SHEET *sht_back;
 
   fifo32_init(&fifo, 128, fifobuf);
   timer_ts = timer_alloc();
   timer_init(timer_ts, &fifo, 1);
   timer_settime(timer_ts, 2);
+  sht_back = (struct SHEET *) *((int *) 0x0fec);
 
   for (;;) {
+    count++;
+    my_sprintf(s, "%d", count);
+    putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
     io_cli();
     if (fifo32_status(&fifo) == 0) {
       io_sti();
-      io_hlt();
     } else {
       i = fifo32_get(&fifo);
       io_sti();
