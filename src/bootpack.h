f6c356125e0057caa46952c8c1eb7733d311476f
urawa72 <urawa72@gmail.com>
2021-08-27 11:58:48 +0900

    harib13a

--- src/bootpack.h
+++ src/bootpack.h
@@ -185,6 +185,26 @@ void timer_settime(struct TIMER *timer, unsigned int timeout);
 void inthandler20(int *esp);
 
 /* mtask.c */
-extern struct TIMER *mt_timer;
-void mt_init(void);
-void mt_taskswitch(void);
+#define MAX_TASKS 1000
+#define TASK_GDT0 3 // from what number of GDT to assign TSS
+struct TSS32 {
+  int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+  int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  int es, cs, ss, ds, fs, gs;
+  int ldtr, iomap;
+};
+struct TASK {
+	int sel, flags;  // sel means number of GDT
+	struct TSS32 tss;
+};
+struct TASKCTL {
+	int running;  // number of working tasks
+	int now; // which task is now working
+	struct TASK *tasks[MAX_TASKS];
+	struct TASK tasks0[MAX_TASKS];
+};
+extern struct TIMER *task_timer;
+struct TASK *task_init(struct MEMMAN *memman);
+struct TASK *task_alloc(void);
+void task_run(struct TASK *task);
+void task_switch(void);
--- src/timer.c
+++ src/timer.c
@@ -98,7 +98,7 @@ void inthandler20(int *esp) {
     }
     // timeout
     timer->flags = TIMER_FLAGS_ALLOC;
-    if (timer != mt_timer) {
+    if (timer != task_timer) {
       fifo32_put(timer->fifo, timer->data);
     } else {
       ts = 1;  // mt_timer is timeout
@@ -108,7 +108,7 @@ void inthandler20(int *esp) {
   timerctl.t0   = timer;
   timerctl.next = timerctl.t0->timeout;
   if (ts != 0) {
-    mt_taskswitch();
+    task_switch();
   }
   return;
 }
