71665ba106631eaf2649247268b158aa58ba3cbc
urawa72 <urawa72@gmail.com>
2021-08-28 21:08:00 +0900

    harib14e

--- src/bootpack.c
+++ src/bootpack.c
@@ -9,28 +9,40 @@ void console_task(struct SHEET *sheet);
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   struct FIFO32 fifo;
+  struct SHTCTL *shtctl;
   char s[40];
   int fifobuf[128];
   int mx, my, i, cursor_x, cursor_c;
   unsigned int memtotal;
   struct MOUSE_DEC mdec;
   struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
-  struct SHTCTL *shtctl;
+  unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
+  struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
+  struct TASK *task_a, *task_cons;
+  struct TIMER *timer;
   // clang-format off
-  static char keytable[0x54] = {
+  static char keytable0[0x80] = {
     0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
     'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
     'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
     0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
-    '2', '3', '0', '.'
+    '2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0x5c, 0,  0,   0,   0,   0,   0,   0,   0,   0,   0x5c, 0,  0
+  };
+  static char keytable1[0x80] = {
+    0,   0,   '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~', 0,   0,
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{', 0,   0,   'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', '+', '*', 0,   0,   '}', 'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', '<', '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+    '2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
   };
   // clang-format on
-  unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
-  struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
-  struct TASK *task_a, *task_cons;
-  struct TIMER *timer;
-  int key_to = 0;
+  int key_to = 0, key_shift = 0;
 
   // initialize IDT/PIC
   init_gdtidt();
@@ -125,16 +137,24 @@ void HariMain(void) {
       if (256 <= i && i <= 511) {  // keyboard data
         my_sprintf(s, "%x", i - 256);
         putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-        if (i < 0x54 + 256 && keytable[i - 256] != 0) {  // normal charactor
+        if (i < 0x80 + 256) {  // convert key code to charactor code
+          if (key_shift == 0) {
+            s[0] = keytable0[i - 256];
+          } else {
+            s[0] = keytable1[i - 256];
+          }
+        } else {
+          s[0] = 0;
+        }
+        if (s[0] != 0) {  // normal charactor
           if (key_to == 0) {
             if (cursor_x < 128) {
-              s[0] = keytable[i - 256];
               s[1] = 0;
               putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1);
               cursor_x += 8;
             }
           } else {  // to console
-            fifo32_put(&task_cons->fifo, keytable[i - 256] + 256);
+            fifo32_put(&task_cons->fifo, s[0] + 256);
           }
         }
         if (i == 256 + 0x0e) {  // backspace
@@ -159,6 +179,18 @@ void HariMain(void) {
           }
           sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
           sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+        }
+        if (i == 256 + 0x2a) {  // left shift ON
+          key_shift |= 1;
+        }
+        if (i == 256 + 0x36) {  // right shift ON
+          key_shift |= 2;
+        }
+        if (i == 256 + 0xaa) {  // left shift OFF
+          key_shift &= ~1;
+        }
+        if (i == 256 + 0xb6) {  // right shift OFF
+          key_shift &= ~2;
         }
         // redisplay cursor
         boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
