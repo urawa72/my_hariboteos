7d4dadac7422c1edd94d05c5643defe67f1d7f8c
urawa72 <urawa72@gmail.com>
2021-08-29 13:01:46 +0900

    harib15c

--- src/bootpack.c
+++ src/bootpack.c
@@ -183,6 +183,11 @@ void HariMain(void) {
             fifo32_put(&task_cons->fifo, 8 + 256);
           }
         }
+        if (i == 256 + 0x1c) {  // enter
+          if (key_to != 0) {
+            fifo32_put(&task_cons->fifo, 10 + 256);
+          }
+        }
         if (i == 256 + 0x0f) {  // tab
           if (key_to == 0) {
             key_to = 1;
@@ -385,7 +390,7 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
 void console_task(struct SHEET *sheet) {
   struct TIMER *timer;
   struct TASK *task = task_now();
-  int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
+  int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
   char s[2];
 
   fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -422,29 +427,36 @@ void console_task(struct SHEET *sheet) {
         cursor_c = COL8_FFFFFF;
       }
       if (i == 3) {  // cursor off
-        boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, 28, cursor_x + 7, 43);
+        boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
         cursor_c = -1;
       }
-      if (256 <= i && i <= 511) {  // for keyboard
-        if (i == 8 + 256) {
+      if (256 <= i && i <= 511) {  // for keyboard via task_a
+        if (i == 8 + 256) {        // backspace
           if (cursor_x > 16) {
-            putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, " ", 1);
+            putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
             cursor_x -= 8;
           }
-        } else {
+        } else if (i == 10 + 256) {  // enter
+          if (cursor_y < 28 + 112) {
+            putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+            cursor_y += 16;
+            putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">", 1);
+            cursor_x = 16;
+          }
+        } else {  // normal charactor
           if (cursor_x < 240) {
             s[0] = i - 256;
             s[1] = 0;
-            putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, s, 1);
+            putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
             cursor_x += 8;
           }
         }
       }
       if (cursor_c >= 0) {
         // redisplay cursor
-        boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+        boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
       }
-      sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
+      sheet_refresh(sheet, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
     }
   }
 }
